56574614dfbf79f89ea6c412cb14aa3513013fa8
Andrew Ogden <thisisandrewogden@gmail.com>
2019-05-22 14:44:06 -0700

    serverless deploy

--- backend/ebs/handler.ts
+++ backend/ebs/handler.ts
@@ -2,10 +2,11 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
+  console.log("Andrew sample log output")
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      message: 'AO Callback success!',
       input: event,
     }, null, 2),
   };
